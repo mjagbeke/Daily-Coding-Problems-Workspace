6d20ee089680cc1af7e7f96ec8aa71fc3a259ceb
Julian Agbeke <mjagbeke@mtu.edu>
2019-08-14 22:51:58 -0400

    Created header files for structs neeeded

---
+++ Daily_Coding_Problems_C/DCP_3/src/Root.h
@@ -0,0 +1,13 @@
+/*
+ * Root.h
+ *
+ *  Created on: Aug 14, 2019
+ *      Author: julian_home
+ */
+
+#ifndef ROOT_H_
+#define ROOT_H_
+
+
+
+#endif /* ROOT_H_ */
---
+++ Daily_Coding_Problems_C/DCP_3/src/Tree.h
@@ -0,0 +1,13 @@
+/*
+ * Tree.h
+ *
+ *  Created on: Aug 14, 2019
+ *      Author: julian_home
+ */
+
+#ifndef TREE_H_
+#define TREE_H_
+
+
+
+#endif /* TREE_H_ */
